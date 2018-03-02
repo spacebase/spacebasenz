69d5f59846e43f78ee5ea201e8df87e2c190a192
Richard Bodo <rich@spacebase.co>
2018-03-02 02:33:10 +0000

    prod_03012018_1

--- web/themes/custom/spacebase/js/custom.js
+++ web/themes/custom/spacebase/js/custom.js
@@ -7,7 +7,7 @@
       
     
       var clickTap = $.support.touch ? "tap" : "click";
-
+/*
       $('a[href*="#"]:not([href="#"])').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -20,7 +20,7 @@
           }
         }
       });
-      
+*/      
       window.FontAwesomeConfig = {
         searchPseudoElements: true
       }
@@ -47,6 +47,30 @@
       
       $(document).click(function(e){
         $('.user-dropdown').fadeOut(300);
+      });
+
+      $(document).on('show.bs.tab', '.nav-tabs-responsive [data-toggle="tab"]', function(e) {
+        var $target = $(e.target);
+        var $tabs = $target.closest('.nav-tabs-responsive');
+        var $current = $target.closest('li');
+        var $parent = $current.closest('li.dropdown');
+    		$current = $parent.length > 0 ? $parent : $current;
+        var $next = $current.next();
+        var $prev = $current.prev();
+        var updateDropdownMenu = function($el, position){
+          $el
+          	.find('.dropdown-menu')
+            .removeClass('pull-xs-left pull-xs-center pull-xs-right')
+          	.addClass( 'pull-xs-' + position );
+        };
+    
+        $tabs.find('>li').removeClass('next prev');
+        $prev.addClass('prev');
+        $next.addClass('next');
+        
+        updateDropdownMenu( $prev, 'left' );
+        updateDropdownMenu( $current, 'center' );
+        updateDropdownMenu( $next, 'right' );
       });
       
     }

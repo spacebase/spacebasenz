335c8520884721fb436b9a4ff279c6b24ceff745
Stephen Cataldo <stephencataldo@gmail.com>
2018-04-17 16:24:36 -0600

    Forums: Subtask #88, Build out the nav system. Bascially working; this commit includes some direction changes and serious cleanup needed.

---
+++ web/modules/custom/spacebase_core/src/Plugin/Block/OrgMenuBlock.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Drupal\spacebase_core\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'OrgMenuBlock' block.
+ *
+ * @Block(
+ *  id = "org_menu_block",
+ *  admin_label = @Translation("Org menu block"),
+ * )
+ */
+class OrgMenuBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+    $build['org_menu_block']['#markup'] = 'Implement OrgMenuBlock.';
+    $build['#theme'] = 'menu_org_profile';
+
+
+    $path = explode('/',\Drupal::service('path.current')->getPath());
+    $build['#gid'] = intval($path[2]);
+    $a = $path[3];
+    $active = [];
+    $active[$a] = 'active';    // @ToDo standdardize?
+    $build['#active'] = $active;
+    return $build;
+  }
+}

0a20c4d29690bb6ed5c7e83766492e14def5939b
Mike Hoffman <mike@137px.com>
2018-05-22 13:13:39 -0400

    advanced search styling

--- web/modules/custom/spacebase_core/js/sb-search.js
+++ web/modules/custom/spacebase_core/js/sb-search.js
@@ -5,18 +5,21 @@
 
         if (localStorage.getItem("orgAdvancedSearchEnabled") == 'true') {
           $('a#org-advanced-search').addClass('label-default');
-          $('#block-orgadvancedsearchblock').show();
+          $('#block-orgadvancedsearchblock').slideDown(300);
+          $('body').addClass('topbar-open');
         }
 
         $('a#org-advanced-search').click(function() {
           $(this).toggleClass('label-default');
           if ($(this).hasClass('label-default')) {
             localStorage.setItem("orgAdvancedSearchEnabled", 'true');
-            $('#block-orgadvancedsearchblock').show();
+            $('#block-orgadvancedsearchblock').slideDown(300);
+            $('body').addClass('topbar-open');
           }
           else {
             localStorage.setItem("orgAdvancedSearchEnabled", 'false');
-            $('#block-orgadvancedsearchblock').hide();
+            $('#block-orgadvancedsearchblock').slideUp(300);
+            $('body').removeClass('topbar-open');
           }
         });
       }
